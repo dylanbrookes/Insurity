d9cac35a95d8674c7cbdcd87d8081960fb14db49
Dylan Brookes <dylan.brookes@live.com>
2019-08-24 12:36:57 -0400

    landing page

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { layout: false, title: 'Express' });
+  res.render('index', { layout: false, title: 'Express'});
 });
 
 router.get('/demo', function(req, res, next) {
   res.render('demo', { layout: false, title: 'Demo' });
 });
+
+router.get('/dashboard', function(req, res, next) {
+  res.render('dashboard', { layout: false, title: 'Dashboard' });
+});
+
+
 
 module.exports = router;
