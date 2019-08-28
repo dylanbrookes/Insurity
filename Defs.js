413bc83f7d6c9e0e5bcd3f45dd028788bba425b4
ryanrossiter <nayr0788@gmail.com>
2019-08-28 19:51:01 -0400

    Update Defs.js

--- Defs.js
+++ Defs.js
@@ -1,6 +1,6 @@
 module.exports = {
 	AWS_CREDENTIALS: {
-		accessKeyId: 'AKIATN5EMPDILZHVZE45',
-		secretAccessKey: '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'
+		accessKeyId: '***',
+		secretAccessKey: '***'
 	}
 }
